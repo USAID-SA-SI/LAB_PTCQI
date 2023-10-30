a1f74c4984bbb000ed27fe99e1017ce68560a94e
ctrapence <trapenceclement@gmail.com>
2023-10-30 17:03:40 +0200

    Update LAB_PTCQI.R
    FY23Q4 Data import

--- LAB_PTCQI.R
+++ LAB_PTCQI.R
@@ -1,8 +1,9 @@
 # Title: LAB_PCTQI MER Reporting Script
 # Author: C. Trapence
 # Purpose: Automating the process of Reporting!
+# Updated:Rosaline & Clement
 # Date:2022-10-20
-# Modified:2023-09-08
+# Modified:2023-10-30
 
 #Load Required libraries
 library(tidyverse)
@@ -12,6 +13,7 @@ library(readr)
 library(excel.link)
 library(openxlsx)
 library(data.table)
+library(here)
 
 current_quarter<-"FY23Q4"
 
@@ -27,38 +29,51 @@ genie<-list.files(here("Data/genie"),pattern="SITE")
 
 DATIM<-read.delim2(here("data/genie",genie))
 
+#Data_elements2<-read.csv("C:/Users/rpineteh/Documents/data/Data sets, elements and combos paramaterized.csv")
 
-Data_elements<-read.csv("Data/Data sets, elements and combos paramaterized.csv")
+DE<-list.files(here("data"),pattern = "elements")
 
-LAB_PTQI<-DATIM %>% filter(sitetype=="Facility",indicator=="HTS_TST",fiscal_year==2023 ,funding_agency=="USAID",standardizeddisaggregate %like% "Total Numerator")
+70
+#genie<-read.delim2("C:/Users/rpineteh/Documents/data/Genie-SITE_IM-South Africa-Daily-2023-10-30.txt")
 
-LAB_PTQIv1<-LAB_PTQI %>%   select(facility,orgunituid,psnu,psnuuid,cumulative,mech_code,mech_name,categoryoptioncomboname,qtr1,qtr2,qtr3,qtr4,categoryoptioncombouid,dataelementuid) %>%
-  group_by(facility,orgunituid,psnu,psnuuid,mech_code,mech_name,categoryoptioncomboname,categoryoptioncombouid,dataelementuid) %>%  mutate(cumulative=as.integer(cumulative),qtr1=as.integer(qtr1),qtr2=as.integer(qtr2),qtr3=as.integer(qtr3),qtr4=as.integer(qtr4)) %>% 
-  summarise_at(vars(qtr1,qtr2,qtr3,qtr4,cumulative),sum,na.rm=TRUE) %>%  mutate(Value=cumulative) %>% 
-mutate(CategoryOptionCombo="LAB_PTCQI_N_NoApp_POCT_TestVolume",dataelementuid ="KMtAtCRNZl8",categoryoptioncombouid="oCr3aOvULR9",Period="2023Q3") %>% 
-  
-  rename(OrgUnit="orgunituid")
+Data_elements3<-read.csv(here("Data", DE))
+
+MC<- list.files(here("data"),pattern= "Mechanism")
+
+mechanisms<- read.csv(here("Data", MC)) %>% filter(ou=="South Africa") %>% rename(AttributeOptionCombo=uid,mech_code=code) %>% mutate(mech_code=as.character(mech_code))
+
+
+LAB_PTQI<-DATIM %>% filter(indicator=="HTS_TST",fiscal_year==2023 ,funding_agency=="USAID",standardizeddisaggregate %like% "Total Numerator")
+write.xlsx(LAB_PTQI,"Data_view4.xlsx")
+
+
+LAB_PTQIv1<-LAB_PTQI %>%   select(facility,orgunituid,psnu,psnuuid,cumulative,mech_code,mech_name,categoryoptioncomboname,qtr1,qtr2,qtr3,qtr4,categoryoptioncombouid,dataelementuid) %>% 
+  group_by(facility,orgunituid,psnu,psnuuid,mech_code,mech_name,categoryoptioncomboname,categoryoptioncombouid,dataelementuid)  %>%  mutate(cumulative=as.integer(cumulative),qtr1=as.integer(qtr1),qtr2=as.integer(qtr2),qtr3=as.integer(qtr3),qtr4=as.integer(qtr4)) %>% 
+  summarise_at(vars(qtr1,qtr2,qtr3,qtr4,cumulative),sum,na.rm=TRUE) %>%  mutate(Value=qtr1+qtr2+qtr3+qtr4) %>% 
+mutate(CategoryOptionCombo="LAB_PTCQI_N_NoApp_POCT_TestVolume",dataelementuid ="KMtAtCRNZl8",categoryoptioncombouid="oCr3aOvULR9",Period="2023Q3") %>%   rename(OrgUnit="orgunituid")
+
+
 
 #Joining this file with the Data sets, elements and combos paramaterized to get the UID's 
-LAB_PTQIv2<-left_join(LAB_PTQIv1,Data_elements,by=c("dataelementuid","categoryoptioncombouid"))
+#LAB_PTQIv2<-left_join(LAB_PTQIv1,Data_elements,by=c("dataelementuid","categoryoptioncombouid"))
 
-LAB_PTQIv2.1<-LAB_PTQIv2 %>%  select(dataelementuid,Period,OrgUnit,mech_code,categoryoptioncombouid,Value) %>% filter(mech_code %in% c("70310","70290","70287","81902"))%>%  
+LAB_PTQIv2.1<-LAB_PTQIv1 %>%  select(dataelementuid,Period,OrgUnit,mech_code,categoryoptioncombouid,Value) %>% filter(mech_code %in% c("70310","70290","70287","81902", "70301"))%>%  
   rename(Dataelement=dataelementuid,CategoryOptionCombo=categoryoptioncombouid)
 
-# #Lining with the mechanism to get the  Mechanism Attribute Combo Option UIDs
 
-mechanisms<-read.csv("data/Mechanisms partners agencies OUS Start End.csv") %>% filter(ou=="South Africa") %>% rename(AttributeOptionCombo=uid,mech_code=code) %>% mutate(mech_code=as.character(mech_code))
+# #Lining with the mechanism to get the  Mechanism Attribute Combo Option UIDs
 
 LAB_PTQIv2.2<-left_join(LAB_PTQIv2.1,mechanisms,by="mech_code") %>%  select(Dataelement ,Period,OrgUnit,CategoryOptionCombo,AttributeOptionCombo, Value) 
 
 
+write.xlsx(LAB_PTQIv2.2,"Data_view9.xlsx")
 
 LAB_PTQIv2.3<-LAB_PTQIv2.2%>% group_by(Dataelement ,Period,OrgUnit,CategoryOptionCombo,AttributeOptionCombo) %>% summarise_at(vars(Value),sum,na.rm=TRUE) 
 
-
+write.xlsx(LAB_PTQIv2.3,"Data_view10.xlsx")
 # Export CSV in readiness for import
 
-shell("taskkill /im EXCEL.exe /f /t")
-filename<-paste(Sys.Date(),current_quarter,"LAB_PTCQI.csv")
+#shell("taskkill /im EXCEL.exe /f /t")
+filename<-paste0(current_quarter,"_LAB_PTCQI_",Sys.Date(),".csv")
 
-write.csv(LAB_PTQIv2.3,filename,row.names = FALSE)
+write.csv(LAB_PTQIv2.3,here("dataout",filename),row.names = FALSE)
